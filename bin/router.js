091105448dca3aa5c44c7c87a30a2cdc6be35395
jcogollo95 <jcogollorudino@correo.unicordoba.edu.co>
2020-10-01 15:53:39 +0000

    adelanto

---
+++ bin/router.js
@@ -0,0 +1,9 @@
+const express = requiere("expreeess");
+const app = express();
+
+app.get("/", function (req, res){ 
+    res.status(200).send("hola mundo")
+
+})
+
+exports.app = app;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hola mundo");
+const {app} = requiere("./bin/router");
+
+app.listen(3000, ()=>{
+    console.log("server start at post 3000");
+
+});
