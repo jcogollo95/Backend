b9a68a13163e5cee6b40cc4ace09e183ebd401c3
jcogollo95 <jcogollorudino@correo.unicordoba.edu.co>
2020-10-07 20:10:38 +0000

    adelanto

--- bin/router.js
+++ bin/router.js
@@ -4,7 +4,20 @@ const app = express();
 app.get("/", function (req, res){ 
     res.status(200).send("hola mundo");
 
-})
+});
+
+app.get("/users", function (req, res) {
+    let users = [
+        {nickname: "josecogollo", password: "12345"},
+        {nickname: "Alixbaron", password: "12345678"},
+        {nickname: "GreyNovoa", password: "1234567890"},
+        {nickname: "PaulaRambao", password: "12345"},
+        {nickname: "SebastianPaternina", password: "12345"}
+        
+    ];
+    res.status(200).send(users);
+    
+});
 
 
 exports.app = app;
