96f2cca464f32c350c1ab5ae7d8f566c2213ac03
jcogollo95 <jcogollorudino@correo.unicordoba.edu.co>
2020-10-13 16:23:56 +0000

    configuracion servidor

--- bin/router.js
+++ bin/router.js
@@ -21,27 +21,32 @@ app.get("/users", function (req, res) {
 
 app.get("/songs", function (req, res) {
     let songs = [
-        {id: "hdh76hy6h44r4", nombre: "la risa de las vocales", duracion: "4:20", id_autor: "cgcb87uy6f"},
-        {id: "hdh76hy6h44r4", nombre: "la cancion de los colores", duracion: "3:10", id_autor: "cgcb87uy6f"},
-        {id: "hdh76hy6h44r4", nombre: "las figuras geometricas", duracion: "5:15", id_autor: "kh6hf6fh9j8"}
+        {id: "hdh76hy6h44r4", nombre: "la risa de las vocales", duracion: "4:20", id_autor: "cgcb87uy6f"}, 
+        {id: "hdgjr4dmy6hdk", nombre: "la cancion de los colores", duracion: "3:10", id_autor: "cgcb87uy6f"},
+        {id: "hhs7ujfnd8s8s", nombre: "las figuras geometricas", duracion: "5:15", id_autor: "kh6hf6fh9j8"}
     ];
     res.status(200).send(songs);
 
     
 });
+app.get("/songs/:id", function (req, res) {
+    let { id } = req.params
 
-app.get("/song/:id", function (req, res) {
-    let { id } = req.params;
     let song;
-    if(id==="hdh76hy6h44r4"){
+    if (id==="hdh76hy6h44r4") {
         song = {id: "hdh76hy6h44r4", nombre: "la risa de las vocales", duracion: "4:20", id_autor: "cgcb87uy6f"}
-    }else{
-        song ={id: "hdh76hy6h44r4", nombre: "la cancion de los colores", duracion: "3:10", id_autor: "cgcb87uy6f"}
 
- 
+        
+    }else if(id === "hdgjr4dmy6hdk"){
+        song = {id: "hdgjr4dmy6hdk", nombre: "la cancion de los colores", duracion: "3:10", id_autor: "cgcb87uy6f"}
+
+    }else{
+        res.status(404).send({message: "La cancion no esta registrada en la base de datos"})
     }
     res.status(200).send(song);
     
 });
+
+
 
 exports.app = app;
